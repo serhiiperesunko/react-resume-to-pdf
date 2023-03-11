846b4b5925e89da749ed3db8fe486f9b9809ffc6
sergiy <serhiiperesunko@gmail.com>
2023-03-11 13:58:07 +0200

    update

--- src/App.js
+++ src/App.js
@@ -1,119 +1,27 @@
-import './App.css';
 import {usePDF} from "./hook/usePDF";
+import './App.css'
 import {useState} from "react";
 
+const App = () => {
+    const {CovertToPDF, onGeneratePDF} = usePDF()
+    const [html, setHTML] = useState(`<div style="padding:20px;"><h1>Hello, World!</h1><p>This is a simple example of CSS in a separate file.</p><ul><li><a href="#">Home</a></li><li><a href="#">About</a></li><li><a href="#">Contact</a></li></ul><div><h2>Welcome to my site!</h2><p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer gravida, enim a commodo dapibus, enim tellus varius nulla, a dictum tortor magna non massa. Nulla facilisi. Aenean sit amet enim vel elit dapibus consectetur.</p></div></div></div>`)
 
-const ResumeTemplate = ({type} = 1) => {
-    if (type === 1) {
-        return <div className="template a4 one">
-            <h1>Thomas A. Anderson</h1>
-            <p>
-                (111) 111-1111 | ThomasAnderson@email.com | Los Angeles, CA | Linkedin.com/in/ThomasAnderson
-            </p>
-            <h3>PROFESSIONAL SUMMARY</h3>
-            <p>Write a short summary that highlights your key qualifications, experience, and achievements. Tailor
-                your summary to the job you are applying for and use keywords from the job description. Keep it concise,
-                no more than two or three sentences.
-            </p>
-            <h3>AREAS OF EXCELLENCE</h3>
-            <p>
-                List your areas of expertise and skills that align with the job requirements. Use bullet points or short
-                phrases to make it easy to read and scan quickly. Highlight the skills that make you stand out as a
-                candidate.
-            </p>
-            <h3>PROFESSIONAL EXPERIENCE</h3>
-            <p>
-                Provide your job title.
-                – Provide the name of the company.f
-                List your key responsibilities and accomplishments in bullet points or short phrases. Use action verbs
-                to start each bullet point and quantify your achievements with numbers whenever possible. Highlight your
-                relevant experience that aligns with the job requirements.
-            </p>
-            <h3>EDUCATION & CERTIFICATIONS</h3>
-            <p>
-                Provide the name of the degree or certification. – Provide the name of the institution where you
-                received your degree or certification.
-            </p>
-            <h3>KEY TECHNOLOGIES & TOOLS</h3>
-            <p>
-                List the key technologies and tools you are proficient in, especially those that are relevant to the job
-                you are applying for. Use bullet points or short phrases and group similar tools together. Avoid listing
-                too many items that are not relevant to the job requirements.
-            </p>
-        </div>
-    }
 
-    if (type === 2) {
-        return <div className="template a4 two">
-            <h1>Thomas A. Anderson</h1>
-            <p>
-                (111) 111-1111 | ThomasAnderson@email.com | Los Angeles, CA | Linkedin.com/in/ThomasAnderson
-            </p>
-            <h3>PROFESSIONAL SUMMARY</h3>
-            <p>Write a short summary that highlights your key qualifications, experience, and achievements. Tailor
-                your summary to the job you are applying for and use keywords from the job description. Keep it concise,
-                no more than two or three sentences.
-            </p>
-            <h3>AREAS OF EXCELLENCE</h3>
-            <p>
-                List your areas of expertise and skills that align with the job requirements. Use bullet points or short
-                phrases to make it easy to read and scan quickly. Highlight the skills that make you stand out as a
-                candidate.
-            </p>
-            <h3>PROFESSIONAL EXPERIENCE</h3>
-            <p>
-                Provide your job title.
-                – Provide the name of the company.f
-                List your key responsibilities and accomplishments in bullet points or short phrases. Use action verbs
-                to start each bullet point and quantify your achievements with numbers whenever possible. Highlight your
-                relevant experience that aligns with the job requirements.
-            </p>
-            <h3>EDUCATION & CERTIFICATIONS</h3>
-            <p>
-                Provide the name of the degree or certification. – Provide the name of the institution where you
-                received your degree or certification.
-            </p>
-            <h3>KEY TECHNOLOGIES & TOOLS</h3>
-            <p>
-                List the key technologies and tools you are proficient in, especially those that are relevant to the job
-                you are applying for. Use bullet points or short phrases and group similar tools together. Avoid listing
-                too many items that are not relevant to the job requirements.
-            </p>
+    return <div className="preview">
+        <div className='panel'>
+            <button onClick={onGeneratePDF}>Generate PDF</button>
         </div>
-    }
+        <div className="content">
+            <section className={'convert'}>
+                <CovertToPDF>
+                    <div dangerouslySetInnerHTML={{__html: html}}/>
+                </CovertToPDF>
+            </section>
+            <section>
+                <textarea name="" id="" value={html} onChange={(e) => setHTML(e.target.value)}></textarea>
+            </section>
+        </div>
+    </div>
 }
 
-function App() {
-    const [type, setType] = useState(1)
-    const {generatePDF, ConvertToPDF} = usePDF({
-        margin: 10
-    })
-    return (
-        <div style={{
-            display: "flex",
-            alignItems: 'center'
-        }}>
-            <div className="page">
-                <ConvertToPDF className='pdf-body'>
-                    <ResumeTemplate type={type}/>
-                </ConvertToPDF>
-            </div>
-            <div style={{
-                position: "fixed",
-                right: "25%",
-                display: "flex",
-                flexFlow: "column"
-            }}>
-                <button onClick={() => setType(1)}>template 1</button>
-                <button onClick={() => setType(2)}>template 2</button>
-                <div style={{
-                    marginTop: 10
-                }}>
-                    <button onClick={generatePDF}>Generate</button>
-                </div>
-            </div>
-        </div>
-    );
-}
-
-export default App;
+export default App
--- src/hook/usePDF.js
+++ src/hook/usePDF.js
@@ -1,33 +1,68 @@
-import {useRef} from "react";
+/**
+ * @IMPORTANT!!! This version only for the A4 format
+ *
+ * @ConvertToPDF - include content to be converted.
+ *                 This is a simple div wrapper. You can pass any div attrs
+ *
+ * @generatePDF - action to generate PDF
+ * */
+
 import jsPDF from "jspdf";
+import {useRef} from "react";
 
-export const usePDF = (props) => {
-    const fileName = props?.fileName ?? 'default.pdf'
-    const page = props?.page ?? {
-        orientation: "p",
-        format: [297, 210]
+const styles = {
+    pdfPage: {
+        boxSizing: 'border-box',
+    },
+    a4: {
+        width: "210mm"
+    },
+}
+
+export const usePDF = (props = {}) => {
+    /**
+     * @prop filename - name of pdf file
+     * @prop config jsPDF configuration. !IMPORTANT
+     * */
+    const {
+        filename = 'default',
+        config = {}
+    } = props
+
+    const refPDF = useRef(null)
+
+    const CovertToPDF = ({children, style, ...props}) => {
+        return <div
+            ref={refPDF}
+            style={{
+                ...styles.pdfPage,
+                ...styles.a4,
+                ...style
+            }}
+            {...props}
+        >{children}</div>
     }
-    const ref = useRef()
-    // const scale = 0.252 - (((margin / 1000) * 2.45))
-    const generatePDF = () => {
-        const doc = new jsPDF(page);
-        doc.html(ref.current, {
-            html2canvas: {
-                scale: 0.252
-            },
-            callback: function (doc) {
-                doc.save(fileName);
-            },
 
+    const onGeneratePDF = () => {
+        const doc = new jsPDF({
+            unit: 'pt',
+            compress: true,
+            precision: 10,
+            putOnlyUsedFonts: true,
+            ...config
+        });
+        doc.html(refPDF.current, {
+            html2canvas: {
+                scale: 0.75
+            },
+            callback: function () {
+                doc.save(filename);
+            },
         });
     }
 
-    const ConvertToPDF = ({children, ...rest}) => {
-        return <div ref={ref} {...rest}>{children}</div>
-    }
-
     return {
-        generatePDF,
-        ConvertToPDF
+        CovertToPDF,
+        onGeneratePDF
     }
 }
